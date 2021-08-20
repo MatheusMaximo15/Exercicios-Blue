14163ba0c75e960f9649ac94bc4effea8e38d69e
MatheusMaximo15 <74806715+MatheusMaximo15@users.noreply.github.com>
2021-08-20 01:58:05 -0300

    Update Exercicio3.js

--- Exercicio3.js
+++ Exercicio3.js
@@ -9,30 +9,29 @@ console.log("Banco x");
 
 let saque = parseInt(prompt("Digite o valor desejável para saque:"));
 
+if (saque > 10 && saque < 600) {
 
+cem = parseInt(saque/100);
+saque = saque % 100;
 
+cinquenta = parseInt(saque/50);
+saque = saque % 50;
 
-  if (saque > 10 && saque < 600);{
+dez = parseInt(saque/10);
+saque = saque % 10;
 
-  cem = parseInt(saque/100);
-  saque = saque % 100;
+cinco = parseInt(saque/5);
+saque = saque % 5;
 
-  cinquenta = parseInt(saque/50);
-  saque = saque % 50;
-
-  dez = parseInt(saque/10);
-  saque = saque % 10;
-
-  cinco = parseInt(saque/5);
-  saque = saque % 5;
-
-  um = parseInt(saque/1);
-  saque = saque % 1;
+um = parseInt(saque/1);
+saque = saque % 1;
 
 console.log(`Nota(s) R$ 100,00. ${cem} nota(s)`);
 console.log(`Nota(s) R$ 50,00. ${cinquenta} nota(s)`);
 console.log(`Nota(s) R$ 10,00. ${dez} nota(s)`);
 console.log(`Nota(s) R$ 5,00. ${cinco} nota(s)`);
 console.log(`Nota(s) R$ 1,00. ${um} nota(s)`);
-} 
+} else {
+  console.log("Saque indisponível! Saque disponivel somente a partir de R$10,00 e até R$600,00")
+}
 
