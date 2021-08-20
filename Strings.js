6c35550e99bf0c7700535912d24049555ed2ffff
MatheusMaximo15 <74806715+MatheusMaximo15@users.noreply.github.com>
2021-08-19 21:47:02 -0300

    Delete Strings.js

--- Strings.js
+++
@@ -1,5 +0,0 @@
-//Aprendendo Strings
-let mensagem = "Olá, Bluemersssss";
-console.log(`O tamanho da String mensagem é ${mensagem.length} letras`)
-//Toda escrita dentro de aspas é considerada uma string (um texto) 
-//O comando .lenght no final de uma variável, puxa o comprimento de uma string, contando letras e espaçamentos.
