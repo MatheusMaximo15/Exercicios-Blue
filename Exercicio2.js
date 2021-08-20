9bc5200a384f6ab875a94dc4d06fa2caa35b0652
MatheusMaximo15 <74806715+MatheusMaximo15@users.noreply.github.com>
2021-08-19 22:20:49 -0300

    Update Exercicio2.js

--- Exercicio2.js
+++ Exercicio2.js
@@ -1,6 +1,6 @@
 console.log("Jogo de adivinhação")
 let numeros = Math.floor(Math.random(0,10) * 11);
-let resultado = +prompt("Digite um número");
+let resultado = parseInt(prompt("Digite um número"));
 console.log(`O computador escolheu ${numeros}`);
 
 if (resultado === numeros) {
